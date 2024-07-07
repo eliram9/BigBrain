d0193cdd93637301061cd21649ba9a9351eccf62
eliram9 <eliram9@gmail.com>
2024-07-07 13:22:34 -0400

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Resources from './components/pages/Resources.jsx';
+import Contact from './components/pages/Contact';
+import EMDR from './components/pages/EMDR';
+import Schedule from './components/pages/Schedule';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='flex flex-col'>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/emdr" element={<EMDR />} />
+                <Route path="/resources" element={<Resources />} />
+                <Route path="/contact-us" element={<Contact />} />
+                <Route path="/schedule" element={<Schedule />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
