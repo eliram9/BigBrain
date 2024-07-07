c0e64c28cb5d8aadadb93faccc29772cacb27e19
eliram9 <eliram9@gmail.com>
2024-07-07 13:22:13 -0400

    new project

--- tailwind.config.js
+++ tailwind.config.js
@@ -74,31 +74,3 @@ module.exports = {
       require('tailwindcss-filters'),
   ],
 }
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
-import Navbar from './components/Navbar'
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Resources from './components/pages/Resources.jsx';
-import Contact from './components/pages/Contact';
-import EMDR from './components/pages/EMDR';
-import Schedule from './components/pages/Schedule';
-
-function App() {
-    return (
-        <div className='flex flex-col'>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/emdr" element={<EMDR />} />
-                <Route path="/resources" element={<Resources />} />
-                <Route path="/contact-us" element={<Contact />} />
-                <Route path="/schedule" element={<Schedule />} />
-            </Routes>
-        </div>
-    );
-}
-
-export default App;
