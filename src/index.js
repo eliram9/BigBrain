3031043a2bf05972044cc347d4df66277bd86013
eliram9 <eliram9@gmail.com>
2024-07-07 15:28:10 -0400

    Update homepage and Router basename

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-    {/* <Router basename="/therapy"> */}
+    <Router basename="/BigBrain">
       <App />
     </Router>
   </React.StrictMode>
