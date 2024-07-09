6927f16e4dbe8222599af51161b7ccb6564527c0
eliram9 <eliram9@gmail.com>
2024-07-09 12:20:46 -0400

    GoUp button

---
+++ src/components/ui-elements/GoUpButton.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { GoUp, UpArrow } from '../../media/icons';
+
+const GoUpButton = () => {
+    const [showButton, setShowButton] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.pageYOffset > 300) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+        });
+    };
+
+    return (
+        showButton && (
+        <button onClick={scrollToTop}
+                className="fixed bottom-4 right-4 bg-main text-white p-2 rounded-full shadow-lg hover:bg-blue-700 focus:outline-none"
+        >
+            <GoUp />
+        </button>
+        )
+    );
+};
+
+export default GoUpButton;
