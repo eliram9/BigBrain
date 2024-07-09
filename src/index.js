b5f5bffadfed1823f6a9cc1c1014a177081f3ee5
eliram9 <eliram9@gmail.com>
2024-07-09 17:12:47 -0400

    google analytics

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
 import { BrowserRouter as Router } from 'react-router-dom';
-// this is eliram dev branch!
+import { initGA, logPageView } from './analytics';
+import { useEffect } from 'react';
+
+const AppWrapper = () => {
+    useEffect(() => {
+        initGA();
+        logPageView();
+        window.addEventListener('popstate', logPageView);
+        return () => window.removeEventListener('popstate', logPageView);
+    }, []);
+
+    return <App />;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <AppWrapper />
+        </Router>
+    </React.StrictMode>
 );
