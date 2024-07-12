320994c2614afe2d649d6b8ee49ea5a44414e4fa
eliram9 <eliram9@gmail.com>
2024-07-12 08:26:32 -0400

    ga-checks

--- src/analytics.js
+++ src/analytics.js
@@ -1,11 +1,12 @@
-// Google Analytics
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
     ReactGA.initialize('G-M3VZB0DBYL');
+    // console.log('Google Analytics Initialized'); // Debug log
 }
 
 export const logPageView = () => {
     ReactGA.set({ page: window.location.pathname });
     ReactGA.pageview(window.location.pathname);
+    // console.log(`Page view logged for ${window.location.pathname}`); // Debug log
 }
