cd1d299cf75729ed9beb5d18fc842df5e9288f55
eliram9 <eliram9@gmail.com>
2024-08-28 11:59:21 -0400

    added a review and minor style changes

--- src/components/data/reviewsData.js
+++ src/components/data/reviewsData.js
@@ -2,34 +2,40 @@ import woman1 from '../../media/woman1.jpg';
 import man1 from '../../media/man1.jpg';
 import woman2 from '../../media/woman2.jpg';
 import woman3 from '../../media/woman3.jpg';
-import rachel from '../../media/rachel.jpg';
-import ross from '../../media/ross.jpg';
+import woman4 from '../../media/woman4.jpg'
 
 
 const reviewsData = [
     {
         id: 1,
+        name: "Clare",
+        title: "Manassas, VA",
+        text: "I cannot say enough about Arianne and the incredible impact she has had on my life. She has an extraordinary ability to quickly identify patterns in my behavior and, more importantly, explain why I do the things I do. Her expertise in EMDR therapy has been a game-changer for me—I’ve made more progress in a short amount of time than I ever thought possible.My life is incredibly complex right now, with many moving parts, and Arianne has been invaluable in helping me navigate it all. She wears so many hats in our sessions, from a skilled social worker who understands the complexities of managing caregivers for the elderly, to someone who helps me figure out different financial aspects of my situation. Her support has been multifaceted, and I honestly don’t know what I would do without her.Before working with Arianne, I wasn’t thriving—I was merely surviving. But for the first time in my life, I truly believe that thriving is possible. At 55 years old, after working with many therapists, Arianne has helped me tap into a deep source of hope that I didn’t know existed. She has been nothing short of amazing, and I am so grateful to have her by my side on this journey.” Please let me know if I can help any further with this",
+        image: woman4
+    },
+    {
+        id: 2,
         name: "Victoria",
         title: "Washington D.C.",
         text: "I started working with Arianne after the events of Oct 7th and suffering from severe anxiety from them. Arianne is amazing, not only is she empathetic, a great listener and has great suggestions, her EDMR was so effective at changing my thoughts around. We even touched upon other subjects and she has helped improve my life so much!",
         image: woman1
     },
     {
-        id: 2,
+        id: 3,
         name: "Jason",
         title: "Arlington, VA",
         text: "Working with Arianne has been a life-changing experience. Her expertise in EMDR therapy has helped me navigate through some of the darkest periods of my life. Arianne’s empathetic nature and practical advice have been instrumental in my recovery. I feel more empowered and at peace thanks to her guidance and support.",
         image: man1
     },
     {
-        id: 3,
+        id: 4,
         name: "Shira",
         title: "Ra'anana, Israel",
         text: "I don't how I would have gotten through the past few months without Arianne's guidance and support. It has been one of the most challenging times in my life and somehow she really understands what I am going through and is able to listen and also offer helpful practical advice. I am very grateful that I met her and was able to receive the therapy I needed.",
         image: woman2
     },
     {
-        id: 4,
+        id: 5,
         name: "Jeniffer",
         title: "Bethsda, MD",
         text: "Arianne has been an incredible source of support during a very difficult time in my life. Her ability to listen and provide insightful feedback is unparalleled. The EMDR sessions have been transformative, helping me process and heal from past traumas. Arianne’s compassionate and professional approach has truly made a difference in my mental health journey.å",
